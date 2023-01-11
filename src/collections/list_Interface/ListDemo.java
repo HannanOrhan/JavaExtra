8703c882c519738bc2c7e4bd9b8c534747f60037
hannan_Cydeo <hannanorhn00@gmail.com>
2023-01-10 16:25:17 -0800

    set up

---
+++ src/collections/list_Interface/ListDemo.java
@@ -0,0 +1,12 @@
+package collections.list_Interface;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListDemo {
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
+    }
+}
