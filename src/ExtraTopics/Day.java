a9cd45a4402866a79a0bff82808d7a3b88a0e9d8
hannan_Cydeo <hannanorhn00@gmail.com>
2023-01-12 15:06:44 -0800

    Extra topics uodated

---
+++ src/ExtraTopics/Day.java
@@ -0,0 +1,11 @@
+package ExtraTopics;
+
+public enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
---
+++ src/ExtraTopics/TestEnum.java
@@ -0,0 +1,38 @@
+package ExtraTopics;
+
+import java.util.Arrays;
+
+import static ExtraTopics.Day.*;
+
+public class TestEnum {
+    public static void main(String[] args) {
+        //String day = "Monday";
+
+        Day d = Day.Monday;
+
+        switch (d){
+            case Monday:
+                System.out.println("It is monday.");
+                break;
+            case Tuesday:
+                System.out.println("It is monday.");
+                break;
+            case Wednesday:
+                System.out.println("It is monday.");
+                break;
+            case Thursday:
+                System.out.println("It is monday.");
+                break;
+            case Friday:
+                System.out.println("It is monday.");
+                break;
+            case Saturday:
+                System.out.println("It is monday.");
+                break;
+            case Sunday:
+                System.out.println("It is monday.");
+        }
+
+
+    }
+}
