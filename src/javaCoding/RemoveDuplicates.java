33abd01c7232e4a7dd13a53d251361feb9e6ad82
hannan_Cydeo <hannanorhn00@gmail.com>
2023-01-13 16:48:29 -0800

    some updates

--- src/javaCoding/RemoveDuplicates.java
+++ src/javaCoding/RemoveDuplicates.java
@@ -1,7 +1,20 @@
 package javaCoding;
 
-public class RemoveDuplicates {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class RemoveDuplicates {
+    public static String removeDup(String str) {
+        String result = "";
+        for (char c : str.toCharArray()) {
+            if (!result.contains(String.valueOf(c))) {
+                result += c;
+            }
+        }
+        return result;
     }
+        public static void main (String[]args){
+            Scanner input = new Scanner(System.in);
+            String str = input.nextLine();
+            System.out.println( removeDup(str) );
+        }
 }
